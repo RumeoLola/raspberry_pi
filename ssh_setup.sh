c230d1c99be18fbb39811b742d437d31bd6b8811
RumeoLola <rumeomololaz@gmail.com>
2023-03-04 13:49:15 -0500

    linux script need to successfully setup ssh connections

---
+++ ssh_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+
+# run this command on client machine to prepare for ssh connection with raspberrypi
+sudo systemctl start ssh
+
+
+# run this command on the raspberry pi that servers as a ssh server
+sudo systemctl start ssh
+
+
+# I'm still working on this script, haha
